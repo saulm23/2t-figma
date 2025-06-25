91e1c25c71a09f16c465235fa5509992fbf6ec1c
saulm23 <marcasaul00@gmail.com>
2025-06-25 11:48:17 -0400

    componente AddButton terminado

---
+++ components/AddButton.tsx
@@ -0,0 +1,18 @@
+import Link from 'next/link'
+import React from 'react'
+
+const AddButton = () => {
+  return (
+    <div>
+        <Link  className="color-red flex items-center text-white"href="/">
+        Add
+        </Link>
+        
+
+
+
+    </div>
+  )
+}
+
+export default AddButton
